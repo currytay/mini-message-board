708be5307c52a1aaf7a9b612ee7425e7aa46c7f4
currytay <currytay@gmail.com>
2023-12-12 11:40:06 +1100

    Added simple message board functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+/* POST new messages */
+router.post('/new', function(req, res, next) {
+  // Extract message from request body
+  const newMessage = {
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  };
+
+  // Add message to messages array
+  messages.push(newMessage);
+
+  // Redirect back to home page
+  res.redirect('/');
+})
 
 module.exports = router;
